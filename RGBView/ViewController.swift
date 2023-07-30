e4359e4538fe27863f49802eb394ec8b5b36365c
kormishev <kormishev@bk.ru>
2023-07-30 14:59:15 +0300

    release

--- RGBView/ViewController.swift
+++ RGBView/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  RGBView
-//
-//  Created by Mike on 30.07.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet private var colorView: UIView!
+    
+    @IBOutlet private var redValueLabel: UILabel!
+    @IBOutlet private var greenValueLabel: UILabel!
+    @IBOutlet private var blueValueLabel: UILabel!
+    
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(0.6)
+        colorView.layer.cornerRadius = 20
+        changeColorView()
     }
-
-
+    
+    @IBAction private func redValueChanged() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        changeColorView()
+    }
+    @IBAction private func greenValueChanged() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        changeColorView()
+    }
+    @IBAction private func blueValueChanged() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeColorView()
+    }
+    
+    private func changeColorView() {
+        colorView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
